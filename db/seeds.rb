bed4062b4523d581079a5c97e66950ffe72aafa3
turandott <greyvilka@gmail.com>
2022-11-23 17:36:42 +0300

    Initial commit, add controller and model, seeds

---
+++ db/seeds.rb
@@ -0,0 +1,6 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+20.times do |i|
+  Task.create(title: "Task ##{i}", description: "Today that date", status: "progress")
+end
