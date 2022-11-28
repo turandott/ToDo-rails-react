c2c27c38a04ebd751979ed21fb67c9b104871439
turandott <greyvilka@gmail.com>
2022-11-28 17:58:32 +0300

    Add react package and axios

---
+++ todo-app/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+// import TasksContainer from './components/TasksContainer';
+import React,{ Component } from 'react';
+import TodosContainer from "./components/TasksContainer";
+
+class App extends Component {
+    render () {
+        return(
+            <div className="container">
+                <div className="header">
+                    <h1>ToDo list</h1>
+                </div>
+                <TodosContainer/>
+            </div>
+        );
+    }
+}
+
+export default App;
